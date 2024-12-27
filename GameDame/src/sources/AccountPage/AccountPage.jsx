ff5a8efdb8dc346cb549799158bfdaecf0e309fa
Alauana <alauanaj@gmail.com>
2024-12-27 14:08:52 -0300

    refactor. AccountPage okayy

--- GameDame/src/sources/AccountPage/AccountPage.jsx
+++ GameDame/src/sources/AccountPage/AccountPage.jsx
@@ -10,40 +10,51 @@ function AccountPage() {
                 <div className="dados">
                     <div className="text">
                         <h1>Olá, Usuário</h1>
-                        <h2>Seu email é ...</h2>
-                        <h2>Seu CPF é ...</h2>
+                        <h2>Seu email é user@email.com</h2>
+                        <h2>Seu CPF é 000.000.000-00</h2>
                     </div>
                     <div className="buttons">
-                        <button className='alterar'>Alterar Dados <EditIcon fontSize='small'/></button>
-                        <button className='excluir'>Excluir Conta <DeleteForeverIcon fontSize='small'/> </button>
+                        <button className='alterar'>
+                            Alterar Dados 
+                            <div className="editIcon">
+                                <EditIcon fontSize='inherit'/>
+                            </div>
+                        </button>
+                        <button className='excluir'>
+                            Excluir Conta 
+                            <div className="deleteIcon">
+                                <DeleteForeverIcon fontSize='inherit'/> 
+                            </div>
+                        </button>
                         <button className='admin'>Tela do admin</button>
                     </div>
                 </div>
 
-                <div className="games">
-                    <div className="image">
-                        <img src='../../../public/images/bobEsponja.svg' alt='BobEsponja'/>
-                        <br/>
-                        <h4>100/100</h4>
-                    </div> 
-                    <div className="image">
-                        <img src='../../../public/images/Deliver.svg' alt='Deliver'/>
-                        <br/>
-                        <h4>95/100</h4>
-                    </div> 
-                    <div className="image">
-                        <img src='../../../public/images/GodofWar.svg' alt='GodofWar'/>
-                        <br/>
-                        <h4>75/100</h4>
-                    </div> 
-                    <div className="image">
-                        <img src='../../../public/images/deathStranding.svg' alt='deathStranding'/>
-                        <br/>
-                        <h4>Adicionar Avaliação</h4>
-                    </div>                                         
-                </div>
-                <div className="carrinho">
-                    <button className="carrinho"><ShoppingCartOutlinedIcon fontSize='large'/></button>
+                <div className="adquiridos">
+                    <div className="legend">Jogos Adquiridos:</div>
+                
+                    <div className="games">
+                        <div className="image">
+                            <img src='../../../public/images/bobEsponja.svg' alt='BobEsponja'/>
+                            <h4>100/100</h4>
+                        </div> 
+                        <div className="image">
+                            <img src='../../../public/images/Deliver.svg' alt='Deliver'/>
+                            <h4>95/100</h4>
+                        </div> 
+                        <div className="image">
+                            <img src='../../../public/images/GodofWar.svg' alt='GodofWar'/>
+                            <h4>75/100</h4>
+                        </div> 
+                        <div className="image">
+                            <img src='../../../public/images/deathStranding.svg' alt='deathStranding'/>
+                            <h4>Adicionar Avaliação</h4>
+                        </div>                                         
+                    </div>
+                
+                    <div className="carrinho">
+                        <button className="carrinho"><ShoppingCartOutlinedIcon fontSize='large'/></button>
+                    </div>
                 </div>
             </div>    
         </div>
